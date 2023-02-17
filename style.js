8541ecc4678c2f8de24dc783e6d276334ebfb77b
Kviponder <119442089+Kviponder@users.noreply.github.com>
2023-02-16 23:17:01 -0500

    added a fun counter box with the help of ai

---
+++ style.js
@@ -0,0 +1,14 @@
+
+var yesCount = 0;
+var noCount = 0;
+function incrementYes() {
+    yesCount++;
+    document.getElementById("yes-count").innerHTML= yesCount;
+			}
+
+function incrementNo() {
+     noCount++;
+         document.getElementById("no-count").innerHTML= noCount;
+            }
+
+           
